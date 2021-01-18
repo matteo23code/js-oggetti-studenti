d74cea6fa81bd363d9fd92c4e186716491208426
Matteo Basile <matteo.basile56@gmail.com>
2021-01-18 18:09:18 +0100

    es completo

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// var studente = {
+//   'nome': 'Matteo',
+//   'cognome': 'Basile',
+//   'anni': 25,
+// };
+//
+// for (var key in studente ) {
+//   console.log(studente[key]);
+// }
+
+
+var listaStudenti = [
+  {
+  'nome': 'Matteo',
+  'cognome': 'Basile',
+},
+{
+  'nome': 'mario',
+  'cognome': 'rossi',
+},
+{
+  'nome': 'gino',
+  'cognome': 'paoli',
+}
+];
+
+
+var nome = prompt('inserisci il tuo nome.');
+var cognome = prompt('inserisci il tuo cognome.');
+var età = parseInt(prompt('inserisci il tuo età.'));
+
+listaStudenti.push({
+    nome: nome,
+    cognome: cognome,
+    eta: età
+});
+console.log(listaStudenti);
+
+if (isNaN(nome) && isNaN(cognome)) {
+alert('non puoi inserire dei numeri');
+}
